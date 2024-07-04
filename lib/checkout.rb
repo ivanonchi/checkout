556a5ea8c59de21dbbb1529a78b012ff7d4a7b48
Ivan Luque <idluque@gmail.com>
2024-07-04 18:28:41 +0200

    Scan items test

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,13 @@
 class Checkout
-  
+  def initialize
+
+  end
+
+  def scan(code)
+
+  end
+
+  def total
+
+  end
 end
--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -2,8 +2,16 @@ require_relative 'test_helper'
 
 class CheckoutTest < Minitest::Test
   def setup
-    pricing_rules = nil
-    @checkout = Checkout.new(pricing_rules)
+    @checkout = Checkout.new
+  end
+
+  def item_gr1
+    'GR1'
+  end
+
+  def test_scan_item
+    @checkout.scan(item_gr1)
+    assert_equal 1, @checkout.basket.count
   end
 
   def test_buy_one_get_one_free_green_tea
